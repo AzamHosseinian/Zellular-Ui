fe42e8ff3ac23e25872df5688d20d9a96fc398e6
azal <azam.hosseynian@yahoo.com>
2024-07-31 18:06:55 +0330

    update animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography, Button } from "@mui/material";
 import backgroundImage from "../assets/images/HeroSection/background.svg";
 import animationGif from "../assets/images/HeroSection/animation.gif";
@@ -7,30 +7,20 @@ import ArrowIcon from "../assets/images/HeroSection/arrows.svg";
 import "./HeroSection.css";
 
 const HeroSection = () => {
-  const [isGifPlaying, setIsGifPlaying] = useState(true);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [showGif, setShowGif] = useState(false);
-  const heroRef = useRef(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    const img = new Image();
-    img.src = animationGif;
-    img.onload = () => {
-      setIsLoaded(true);
-      setIsGifPlaying(true);
-      setShowGif(true);
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3100); // Adjust the timeout value to match your GIF's duration
 
-      const gifDuration = 3100;
-      setTimeout(() => {
-        setIsGifPlaying(false);
-        setShowGif(false);
-      }, gifDuration);
+    return () => {
+      clearTimeout(timeout);
     };
   }, []);
 
   return (
     <Box
-      ref={heroRef}
       sx={{
         position: "relative",
         display: "flex",
@@ -39,179 +29,175 @@ const HeroSection = () => {
         height: "100vh",
       }}
     >
-      {isLoaded && (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            color: "#FFFFFF",
-            width: "100%",
-            padding: "80px",
-            position: "relative",
-            zIndex: 1,
-          }}
-        >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          color: "#FFFFFF",
+          width: "100%",
+          padding: "80px",
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <Box position="relative">
+          <Box
+            sx={{
+              width: 25,
+              height: 25,
+              backgroundColor: "#92E6A7",
+              borderRadius: "50%",
+              position: "absolute",
+              top: "-5px",
+              left: "-30px",
+              marginTop: "15%",
+            }}
+          />
+          <Typography
+            variant="h3"
+            component="h1"
+            gutterBottom
+            sx={{
+              color: "#FDF5E6",
+              fontWeight: "bold",
+              fontFamily: "Space Grotesk",
+              fontSize: "80px",
+              marginTop: "15%",
+              lineHeight: "0.8",
+            }}
+          >
+            Filling the Gap between
+          </Typography>
+          <Typography
+            variant="h3"
+            component="h1"
+            gutterBottom
+            sx={{
+              color: "#FDF5E6",
+              fontWeight: "bold",
+              fontFamily: "Space Grotesk",
+              fontSize: "80px",
+              display: "inline-block",
+              paddingLeft: "40rem", // space between text and button
+              marginBottom: "5%",
+            }}
+          >
+            Web2 <span style={{ fontFamily: "CabinetGrotesk" }}>&</span> Web3
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#92E6A7",
+              fontSize: "24px",
+              color: "#003A6C",
+              borderRadius: "70px",
+              textTransform: "none",
+              display: "flex",
+              alignItems: "center",
+              fontFamily: "Courier Prime",
+              marginLeft: "45rem",
+              padding: "10px 20px",
+              "&:hover": {
+                bgcolor: "#FDF5E6",
+                color: "#003A6C",
+              },
+            }}
+            endIcon={
+              <Box
+                component="img"
+                src={ArrowIcon}
+                sx={{
+                  width: 24,
+                  height: 24,
+                  ml: 1,
+                }}
+              />
+            }
+          >
+            Build on Zellular
+          </Button>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              color: "#92E6A7",
+              fontFamily: "Courier Prime",
+              fontSize: "40px",
+              textAlign: "left",
+              marginTop: "10%",
+              lineHeight: "1.125",
+            }}
+          >
+            Develop sub-second finality
+            <br />
+            <span style={{ paddingLeft: "15rem" }}>high-throughput dApps</span>
+            <br />
+            <span style={{ paddingLeft: "20rem" }}>
+              in{" "}
+              <span className="typing-effect">any programming language.</span>
+            </span>
+          </Typography>
           <Box position="relative">
             <Box
               sx={{
-                width: 25,
-                height: 25,
-                backgroundColor: "#92E6A7",
-                borderRadius: "50%",
-                position: "absolute",
-                top: "-5px",
-                left: "-30px",
-                marginTop: "15%",
-              }}
-            />
-            <Typography
-              variant="h3"
-              component="h1"
-              gutterBottom
-              sx={{
-                color: "#FDF5E6",
-                fontWeight: "bold",
-                fontFamily: "Space Grotesk",
-                fontSize: "80px",
-                marginTop: "15%",
-                lineHeight: "0.8",
-              }}
-            >
-              Filling the Gap between
-            </Typography>
-            <Typography
-              variant="h3"
-              component="h1"
-              gutterBottom
-              sx={{
-                color: "#FDF5E6",
-                fontWeight: "bold",
-                fontFamily: "Space Grotesk",
-                fontSize: "80px",
-                display: "inline-block",
-                paddingLeft: "40rem", // space between text and button
-                marginBottom: "5%",
-              }}
-            >
-              Web2 <span style={{ fontFamily: "CabinetGrotesk" }}>&</span> Web3
-            </Typography>
-            <Button
-              variant="contained"
-              sx={{
-                bgcolor: "#92E6A7",
-                fontSize: "24px",
-                color: "#003A6C",
-                borderRadius: "70px",
-                textTransform: "none",
                 display: "flex",
                 alignItems: "center",
-                fontFamily: "Courier Prime",
-                marginLeft: "45rem",
-                padding: "10px 20px",
-                "&:hover": {
-                  bgcolor: "#FDF5E6",
-                  color: "#003A6C",
-                },
-              }}
-              endIcon={
-                <Box
-                  component="img"
-                  src={ArrowIcon}
-                  sx={{
-                    width: 24,
-                    height: 24,
-                    ml: 1,
-                  }}
-                />
-              }
-            >
-              Build on Zellular
-            </Button>
-            <Typography
-              variant="subtitle1"
-              sx={{
-                color: "#92E6A7",
-                fontFamily: "Courier Prime",
-                fontSize: "40px",
-                textAlign: "left",
-                marginTop: "10%",
-                lineHeight: "1.125",
+                marginTop: "5%",
               }}
             >
-              Develop sub-second finality
-              <br />
-              <span style={{ paddingLeft: "15rem" }}>
-                high-throughput dApps
-              </span>
-              <br />
-              <span style={{ paddingLeft: "20rem" }}>
-                in{" "}
-                <span className="typing-effect">any programming language.</span>
-              </span>
-            </Typography>
-            <Box position="relative">
               <Box
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  marginTop: "5%",
+                  width: 15,
+                  height: 15,
+                  backgroundColor: "#92E6A7",
+                  borderRadius: "50%",
+                  position: "relative",
+                  marginRight: "10px",
+                  marginLeft: "55%",
+                }}
+              />
+              <Typography
+                sx={{
+                  bgcolor: "transparent",
+                  color: "#FDF5E6",
+                  textTransform: "none",
+                  boxShadow: 0,
+                  borderRadius: 0,
+                  fontFamily: "Courier Prime",
+                  padding: 0,
+                  fontSize: "24px",
                 }}
               >
-                <Box
-                  sx={{
-                    width: 15,
-                    height: 15,
-                    backgroundColor: "#92E6A7",
-                    borderRadius: "50%",
-                    position: "relative",
-                    marginRight: "10px",
-                    marginLeft: "55%",
-                  }}
-                />
-                <Typography
-                  sx={{
-                    bgcolor: "transparent",
-                    color: "#FDF5E6",
-                    textTransform: "none",
-                    boxShadow: 0,
-                    borderRadius: 0,
-                    fontFamily: "Courier Prime",
-                    padding: 0,
-                    fontSize: "24px",
-                  }}
-                >
-                  Secured by EigenLayer
-                </Typography>
-              </Box>
+                Secured by EigenLayer
+              </Typography>
             </Box>
           </Box>
-          {showGif ? (
-            <img
-              src={animationGif}
-              style={{
-                height: "100vh",
-                opacity: 1,
-                transition: "opacity 0.001s",
-              }}
-              alt="Hero Section Animation"
-            />
-          ) : (
-            <img
-              src={finalFrameImage}
-              style={{
-                height: "100vh",
-                opacity: 1,
-                transition: "opacity 0.001s",
-              }}
-              alt="Hero Section Animation"
-            />
-          )}
         </Box>
-      )}
+        {isLoading ? (
+          <img
+            src={animationGif}
+            style={{
+              height: "100vh",
+              opacity: 1,
+              transition: "opacity 0.001s",
+            }}
+            alt="Hero Section Animation"
+          />
+        ) : (
+          <img
+            src={finalFrameImage}
+            style={{
+              height: "100vh",
+              opacity: 1,
+              transition: "opacity 0.001s",
+            }}
+            alt="Hero Section Animation"
+          />
+        )}
+      </Box>
     </Box>
   );
 };
