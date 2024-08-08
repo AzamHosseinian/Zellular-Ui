ea6f3081b25a08008d2a407854c59094b3559f2a
azal <azam.hosseynian@yahoo.com>
2024-08-08 12:20:08 +0330

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Listen on all IPs, or specify your IP
+    port: 8087, // Replace with your desired port
+  },
+});
