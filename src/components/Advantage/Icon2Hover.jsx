7ba1cc4d9021dc0d49f87f80f0488209e0ea88fb
azal <azam.hosseynian@yahoo.com>
2024-08-08 15:23:01 +0330

    update svg icons

---
+++ src/components/Advantage/Icon2Hover.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Icon2Hover = () => (
+  <svg
+    width="178"
+    height="105"
+    viewBox="0 0 178 105"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="52.5" cy="52.5" r="52" stroke="#FDF5E6" />
+    <circle cx="78.5" cy="52.5" r="52" stroke="#FDF5E6" />
+    <circle cx="96.5" cy="52.5" r="52" stroke="#FDF5E6" />
+    <circle cx="109.5" cy="52.5" r="52" stroke="#FDF5E6" />
+    <circle cx="118.5" cy="52.5" r="52" stroke="#FDF5E6" />
+    <circle cx="125.5" cy="52.5" r="52" fill="#FDF5E6" stroke="#FDF5E6" />
+  </svg>
+);
+
+export default Icon2Hover;
---
+++ src/components/Advantage/Icon3Hover.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Icon3Hover = () => (
+  <svg
+    width="155"
+    height="155"
+    viewBox="0 0 155 155"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="77.5" cy="77.5" r="25" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="22" stroke="#FDF5E6" strokeWidth="5" />
+    <circle cx="77.5" cy="77.5" r="27" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="20" fill="#FDF5E6" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="36" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="32" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="29" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="55" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="42" stroke="#FDF5E6" />
+    <circle cx="77.5" cy="77.5" r="77" stroke="#FDF5E6" />
+  </svg>
+);
+
+export default Icon3Hover;
